bd9d8bacd9e4aea9af91a71895a29d5fe44a4977
jrkwsk <ula.jurkowska@gmail.com>
2021-04-18 19:05:36 +0200

    tabs&modes created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PlaylistsView } from './playlists/containers/PlaylistsView';
-
 // npm i bootstrap
 import 'bootstrap/dist/css/bootstrap.css'
 import { MusicSearchView } from './music-search/containers/MusicSearchView';
 
 function App() {
+
+  const [mode, setMode] = useState('showPlaylists')
+
+  const tabs = [
+    { tabText: 'Show my playlists', mode: 'showPlaylists' },
+    { tabText: 'Search for artists', mode: 'searchForArtists' },
+    { tabText: 'Search for albums', mode: 'searchForAlbums' }
+  ]
+
   return (
     <div>
+      <ul className="nav nav-tabs nav-fill">
+        {tabs.map(tab =>
+          <li className="nav-item">
+            <a
+              className="nav-link btn"
+              key={tab.mode}
+              onClick={() => setMode(tab.mode)}>
+              {tab.tabText}</a>
+          </li>
+        )}
+      </ul>
       {/* .container>.row>.col */}
       <div className="container">
         <div className="row">
@@ -15,12 +34,13 @@ function App() {
 
             <h1>MusicApp</h1>
 
-            {/* <PlaylistsView /> */}
-            <MusicSearchView/>
+            {mode === 'showPlaylists' && <PlaylistsView />}
+            {mode === 'artists' && <MusicSearchView />}
+
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
 
