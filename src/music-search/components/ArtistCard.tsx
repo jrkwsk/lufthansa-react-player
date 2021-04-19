6103f3b46cfa5ca2229b85730b9f1310f9bd8294
jrkwsk <ula.jurkowska@gmail.com>
2021-04-19 19:00:06 +0200

    default images on artist search

--- src/music-search/components/ArtistCard.tsx
+++ src/music-search/components/ArtistCard.tsx
@@ -10,7 +10,7 @@ export const ArtistCard = ({ artist }: Props) => {
     return (
         <div className="card h-100">
             {/* <img src="https://www.catsbest.de/wp-content/uploads//katzenbaby-von-der-mutter-trennen-4-1024x684.jpg" className="card-img-top" alt={artist.name} /> */}
-            <img src={artist.images[0]?.url} className="card-img-top" alt={artist.name} />
+            <img src={artist.images[0] ? artist.images[0]?.url : "https://www.catsbest.de/wp-content/uploads//katzenbaby-von-der-mutter-trennen-4-1024x684.jpg"} className="card-img-top" alt={artist.name} />
 
             <div className="card-body">
                 <p className="card-title text-center">{artist.name}</p>
