92d9088bb8da57ff9aa6331be39debc11dfced3f
jrkwsk <ula.jurkowska@gmail.com>
2021-04-19 18:53:38 +0200

    images on artist search done, todo default image if none

--- src/music-search/components/ArtistGrid.tsx
+++ src/music-search/components/ArtistGrid.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { ArtistView } from '../../model/Search'
+import { ArtistView, Artist } from '../../model/Search'
 import { ArtistCard } from './ArtistCard'
 
 interface Props {
-    artists: ArtistView[]
+    artists: ArtistView[] | Artist[]
 }
 
 export const ArtistGrid = ({ artists }: Props) => {
     return (
         <div>
             <div className="row row-cols-1 row-cols-sm-4 no-gutters">
-                {artists.map(artist => <div className="col mb-4" key={artist.id}>
+                {artists.map((artist: ArtistView) => <div className="col mb-4" key={artist.id}>
                     <ArtistCard artist={artist} />
                 </div>)}
             </div>
